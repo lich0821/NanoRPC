cf5b7ee97e407f292023a66414d280fb46f22e45
Changhua <lichanghua0821@gmail.com>
2023-02-15 23:50:26 +0800

    Add FUNC_ENABLE_RECV_TXT

--- Server/server.cpp
+++ Server/server.cpp
@@ -3,13 +3,17 @@
 #include <stdio.h>
 #include <string>
 
+#include <windows.h>
+
 #include <nng/nng.h>
 #include <nng/protocol/pair1/pair.h>
+#include <nng/supplemental/util/platform.h>
 #include <pb_decode.h>
 #include <pb_encode.h>
 
 #include "wcferry.pb.h"
 
+static nng_socket sock;
 std::map<int, std::string> g_types = { { 0x01, "文字" }, { 0x03, "图片" }, { 0x22, "语音" } };
 
 void print_buffer(uint8_t *buffer, size_t len)
@@ -44,7 +48,7 @@ bool func_get_msg_types(uint8_t *out, size_t *len)
     Response rsp              = Response_init_default;
     rsp.func                  = Functions_FUNC_GET_SELF_WXID;
     rsp.which_msg             = Response_types_tag;
-    rsp.msg.types.types_count = g_types.size();
+    rsp.msg.types.types_count = (pb_size_t)g_types.size();
 
     size_t i = 0;
     for (auto it = g_types.begin(); it != g_types.end(); it++) {
@@ -87,6 +91,75 @@ bool func_send_txt(TextMsg txt, uint8_t *out, size_t *len)
     return true;
 }
 
+static DWORD lThreadId      = 0;
+static bool enable_recv_msg = false;
+static int generating_msg()
+{
+    size_t out_len           = 0;
+    static uint8_t out[1024] = { 0 };
+
+    while (1) {
+        nng_msleep(2000);
+        if (enable_recv_msg) {
+            Response rsp           = Response_init_default;
+            rsp.func               = Functions_FUNC_ENABLE_RECV_TXT;
+            rsp.which_msg          = Response_wxmsg_tag;
+            rsp.msg.wxmsg.id       = (char *)"id";
+            rsp.msg.wxmsg.content  = (char *)"content";
+            rsp.msg.wxmsg.is_group = false;
+            rsp.msg.wxmsg.is_self  = false;
+            rsp.msg.wxmsg.sender   = (char *)"sender";
+            rsp.msg.wxmsg.type     = 0x01;
+            if (!pb_get_encoded_size(&out_len, Response_fields, &rsp)) {
+                return false;
+            }
+            pb_ostream_t stream = pb_ostream_from_buffer(out, out_len);
+            if (!pb_encode(&stream, Response_fields, &rsp)) {
+                printf("Encoding failed: %s\n", PB_GET_ERROR(&stream));
+                return false;
+            }
+
+            printf("Send message[%ld]: ", out_len);
+            nng_send(sock, out, out_len, 0);
+            print_buffer(out, out_len);
+        }
+    }
+
+    return 0;
+}
+
+bool func_enable_recv_txt(uint8_t *out, size_t *len)
+{
+    Response rsp   = Response_init_default;
+    rsp.func       = Functions_FUNC_ENABLE_RECV_TXT;
+    rsp.which_msg  = Response_status_tag;
+    rsp.msg.status = 0;
+    if (!enable_recv_msg) {
+        HANDLE rpcThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)generating_msg, NULL, NULL, &lThreadId);
+        if (rpcThread == 0) {
+            printf("Failed to CreateThread\n");
+            rsp.msg.status = -1;
+        }
+        CloseHandle(rpcThread);
+        enable_recv_msg = true;
+    } else {
+        printf("Already enabled.\n");
+    }
+
+    if (!pb_get_encoded_size(len, Response_fields, &rsp)) {
+        return false;
+    }
+
+    pb_ostream_t stream = pb_ostream_from_buffer(out, *len);
+    if (!pb_encode(&stream, Response_fields, &rsp)) {
+        printf("Encoding failed: %s\n", PB_GET_ERROR(&stream));
+        return false;
+    }
+
+    rsp.msg.status = 0;
+    return true;
+}
+
 bool dispatcher(uint8_t *in, size_t in_len, uint8_t *out, size_t *out_len)
 {
     bool ret            = false;
@@ -115,6 +188,11 @@ bool dispatcher(uint8_t *in, size_t in_len, uint8_t *out, size_t *out_len)
             ret = func_send_txt(req.msg.txt, out, out_len);
             break;
         }
+        case Functions_FUNC_ENABLE_RECV_TXT: {
+            printf("[Functions_FUNC_ENABLE_RECV_TXT]\n");
+            ret = func_enable_recv_txt(out, out_len);
+            break;
+        }
         default: {
             printf("[UNKNOW FUNCTION]\n");
             break;
@@ -130,8 +208,7 @@ int main(int argc, char **argv)
     if (argc == 2) {
         url = argv[1];
     }
-    nng_socket sock;
-    nng_listener listener;
+
     int rv;
 
     if ((rv = nng_pair1_open(&sock)) != 0) {
@@ -142,7 +219,9 @@ int main(int argc, char **argv)
         printf("nng_listen: %d\n", rv);
     }
     printf("listening on %s\n", url);
-
+    if ((rv = nng_setopt_ms(sock, NNG_OPT_SENDTIMEO, 1000)) != 0) {
+        printf("nng_setopt_ms: %d\n", rv);
+    }
     static uint8_t out[1024 * 1024] = { 0 };
     while (true) {
         uint8_t *in = NULL;
--- python/client.py
+++ python/client.py
@@ -1,4 +1,3 @@
-import time
 import pynng
 import signal
 from datetime import datetime
@@ -14,33 +13,35 @@ if __name__ == "__main__":
     url = "tcp://127.0.0.1:5555"
     with pynng.Pair1() as sock:
         sock.dial(url)
+
+        req = Request()
+        req.func = 0x01  # FUNC_IS_LOGIN
+        data = req.SerializeToString()
+        print(f"{datetime.now()}: {data}")
+        sock.send(data)
+        rsp = Response()
+        rsp.ParseFromString(sock.recv_msg().bytes)
+        print(f"{rsp}")
+
+        req.func = 0x11  # FUNC_GET_MSG_TYPES
+        data = req.SerializeToString()
+        print(f"{datetime.now()}: {data}")
+        sock.send(data)
+        rsp = Response()
+        rsp.ParseFromString(sock.recv_msg().bytes)
+        print(f"{rsp}")
+
+        req.func = 0x20  # FUNC_SEND_TXT
+        req.txt.msg = "This is message"
+        req.txt.receiver = "Chuck"
+        req.txt.aters = "@all"
+        data = req.SerializeToString()
+        print(f"{datetime.now()}: {data}")
+        sock.send(data)
+        rsp = Response()
+        rsp.ParseFromString(sock.recv_msg().bytes)
+        print(f"{rsp}")
         while True:
-            req = Request()
-            req.func = 0x01  # FUNC_IS_LOGIN
-            data = req.SerializeToString()
-            print(f"{datetime.now()}: {data}")
-            sock.send(data)
             rsp = Response()
             rsp.ParseFromString(sock.recv_msg().bytes)
             print(f"{rsp}")
-
-            req.func = 0x11  # FUNC_GET_MSG_TYPES
-            data = req.SerializeToString()
-            print(f"{datetime.now()}: {data}")
-            sock.send(data)
-            rsp = Response()
-            rsp.ParseFromString(sock.recv_msg().bytes)
-            print(f"{rsp}")
-
-            req.func = 0x20  # FUNC_SEND_TXT
-            req.txt.msg = "This is message"
-            req.txt.receiver = "Chuck"
-            req.txt.aters = "@all"
-            data = req.SerializeToString()
-            print(f"{datetime.now()}: {data}")
-            sock.send(data)
-            rsp = Response()
-            rsp.ParseFromString(sock.recv_msg().bytes)
-            print(f"{rsp}")
-
-            time.sleep(1)
